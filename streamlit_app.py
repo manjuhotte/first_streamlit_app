eab1c02f37f55b5d01eefd6b8fb11ae8676d112b
Manjuhotte <108824955+manjuhotte@users.noreply.github.com>
2022-07-07 06:17:42 +0530

    Update streamlit_app.py
    Added header and sub text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit
+
+
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Idli vada')
+streamlit.text('Dosai')
+streamlit.text('Ricebath')
 
 
 
