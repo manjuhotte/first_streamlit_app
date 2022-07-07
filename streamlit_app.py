5e2f8a61aa1ca6996200a1b0fe0abdbbecdf5216
Manjuhotte <108824955+manjuhotte@users.noreply.github.com>
2022-07-07 06:33:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ streamlit.text('🥙Omega3 and blueberrry oat meal')
 streamlit.text('🧀kale, spinach and rocket smotthie')
 streamlit.text('🍗Hard boiled egg')
 
-streamlit.header(🍧🥧'Build your own best fruit smoothies')
+streamlit.header('🍧🥧Build your own best fruit smoothies')
 
 
 
